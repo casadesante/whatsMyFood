3e99d1645567f9c7c043881897d74a08e29d2672
lakshmantgld <diwaakartg@gmail.com>
2018-03-17 16:24:19 +0900

    ✨ Feat: Firebase JWT Validation
    It checks for JWT token in Authorization heaer of each request. If Valid, then proceeds forward,
    else returns 403

--- serverless/src/index.js
+++ serverless/src/index.js
@@ -1,20 +1,59 @@
 'use strict';
 
 var admin = require('firebase-admin');
-
 var serviceAccount = require('./serviceAccount.json');
 
+// Initialzing the firebase with the appropriate service Account.  
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-exports.http = (request, response) => {
+// Validating the firebase ID token attached with each request.
+const validateFirebaseIdToken = (request) =>
+new Promise((resolve, reject) => {
   console.log('====================================');
-  console.log("Printing request");
+  console.log('Headers:' +  JSON.stringify(request.headers));
   console.log('====================================');
-  response.status(200).send('Hello World!');
-};
 
-exports.event = (event, callback) => {
-  callback();
+  let idToken;
+  if (request.headers.authorization && request.headers.authorization.startsWith('Bearer ')) {
+    console.log('Found "Authorization"  header');
+    // Read the ID Token from the Authorization header.
+    idToken = request.headers.authorization.split('Bearer ')[1];
+  } else {
+    console.error('No Firebase ID token was passed as a Bearer token in the Authorization header.',
+        'Make sure you authorize your request by providing the following HTTP header:',
+        'Authorization: Bearer <Firebase ID Token>',
+        'or by passing a "__session" cookie.');
+    reject("unAuthorized");
+  }
+
+  admin.auth().verifyIdToken(idToken).then((decodedIdToken) => {
+    console.log('ID Token correctly decoded', decodedIdToken);
+    resolve(idToken);
+  }).catch((error) => {
+    console.error('Error while verifying Firebase ID token:', error);
+    reject("unAuthorized");
+  });
+});
+
+
+exports.http = (request, response) => {
+  validateFirebaseIdToken(request)
+  .then((token) => {
+    console.log('====================================');
+    console.log('token: ' + token);
+    console.log('====================================');
+    response.set('Access-Control-Allow-Origin', '*');
+    response.status(200).send(token);  
+  })
+  .catch((error) => {
+    console.error('In Main Catch - error: ' + error);
+    response.set('Access-Control-Allow-Origin', '*');
+    if (error === "unAuthorized") {
+      response.status(403).send(error);
+    } else {
+      response.status(500).send(error);
+    }
+  });
 };
