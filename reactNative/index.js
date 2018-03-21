ea225d6c17126d9d0c133d4bc6c92d81a3412e63
Sharath Vignesh <sharathvignesh08@gmail.com>
2018-03-21 17:55:08 +0900

    🔨 Refactor: Auth flow added
    Added firebase auth for the initial route. Gives the user object if the user has logged in, else
    none.

--- reactNative/index.js
+++ reactNative/index.js
@@ -1,40 +1,4 @@
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  NavigatorIOS,
-  StatusBar
-} from 'react-native';
-import Signin from './src/containers/Signin';
+import { AppRegistry } from 'react-native';
+import App from './src/App';
 
-var styles = StyleSheet.create({
-  container:{
-    flex: 1,
-  },
-});
-
-
-export default class whatsMyFood extends Component {
-  componentDidMount() {
-    StatusBar.setHidden(false);
-  }
-
-  render() {
-    return (
-      <NavigatorIOS
-        initialRoute={{
-          component: Signin,
-          title: 'App\'s first page.',
-          navigationBarHidden: true
-       }}
-        style={styles.container}
-      />
-
-    );
-  }
-}
-
-
-AppRegistry.registerComponent('whatsMyFood', () => whatsMyFood);
+AppRegistry.registerComponent('whatsMyFood', () => App);
