181eab70b39ac8e7b6dc667fa2051c4041d8e667
sharathvignesh <sharathvignesh08@gmail.com>
2018-04-02 14:38:25 +0900

    ✨ Feat: Basic route setup done !
    App's basic route with 4 tabs have been setup

--- reactNative/src/containers/Profile.js
+++ reactNative/src/containers/Profile.js
@@ -10,7 +10,7 @@ export default class Profile extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          New entry !
+          Profile
         </Text>
       </View>
     );
