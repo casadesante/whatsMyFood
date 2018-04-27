1e397893441be8a3d3fc110e487d0685aa0d971a
sharathvignesh <sharathvignesh08@gmail.com>
2018-04-27 14:57:44 +0900

    🔩 Chore: Added data generator helper
    Generates data for restaurant

---
+++ reactNative/src/lib/Helper.js
@@ -0,0 +1,17 @@
+const generateRestaurants = (params = {}) => {
+  const list = [
+    {
+      key: 1,
+      name: 'Mantra',
+    },
+    {
+      key: 2,
+      name: 'T.G.I.F',
+    },
+  ];
+  return list;
+};
+
+module.exports = {
+  generateRestaurants,
+};
