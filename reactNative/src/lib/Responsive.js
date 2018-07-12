774bb1e164163f054d6f9d3adc18b6accc1b5585
Suresh Murali <m.suresh.fx@gmail.com>
2018-07-12 22:44:05 +0900

    🔩 Chore: Eslint cleaned up responsive lib

--- reactNative/src/lib/Responsive.js
+++ reactNative/src/lib/Responsive.js
@@ -1,17 +1,18 @@
-import {Dimensions, PixelRatio} from 'react-native';
-const widthPercentageToDP = widthPercent => {
+import { Dimensions, PixelRatio } from 'react-native';
+
+const widthPercentageToDP = (widthPercent) => {
   const screenWidth = Dimensions.get('window').width;
   // Convert string input to decimal number
   const elemWidth = parseFloat(widthPercent);
   return PixelRatio.roundToNearestPixel(screenWidth * elemWidth / 100);
 };
-const heightPercentageToDP = heightPercent => {
+const heightPercentageToDP = (heightPercent) => {
   const screenHeight = Dimensions.get('window').height;
   // Convert string input to decimal number
   const elemHeight = parseFloat(heightPercent);
-return PixelRatio.roundToNearestPixel(screenHeight * elemHeight / 100);
+  return PixelRatio.roundToNearestPixel(screenHeight * elemHeight / 100);
 };
 export {
   widthPercentageToDP,
-  heightPercentageToDP
+  heightPercentageToDP,
 };
