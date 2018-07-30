191c7a3d012ab2584731a3cbd9d104eb73a3c393
Suresh Murali <m.suresh.fx@gmail.com>
2018-07-30 17:54:22 +0900

    🎨 Style: Responsive signin page

--- reactNative/src/containers/Signin.js
+++ reactNative/src/containers/Signin.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, Image, ImageBackground } from 'react-native';
+import { StyleSheet, Text, Image, ImageBackground, View } from 'react-native';
 import { AccessToken, LoginButton } from 'react-native-fbsdk';
 import LinearGradient from 'react-native-linear-gradient';
-import { Row, Grid } from 'react-native-easy-grid';
 import Config from 'react-native-config';
 import firebase from 'firebase';
 import PropTypes from 'prop-types';
+import { widthPercentageToDP, heightPercentageToDP } from '../lib/Responsive';
+import RF from '../../node_modules/react-native-responsive-fontsize';
 
 const config = {
   apiKey: Config.API_KEY,
@@ -13,6 +14,41 @@ const config = {
   databaseURL: Config.DB_URL,
   storageBucket: 'gs://whatsmyfood.appspot.com',
 };
+
+const styles = StyleSheet.create({
+  linearGradient: {
+    flex: 1,
+    zIndex: 2,
+  },
+  backgroundImage: {
+    flex: 1,
+    height: null,
+    width: null,
+    zIndex: 1,
+  },
+  container: {
+    width: widthPercentageToDP('100%'),
+    marginTop: heightPercentageToDP('15.5%'),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  appTitle: {
+    color: 'white',
+    fontSize: RF(4),
+    marginTop: heightPercentageToDP('4%'),
+  },
+  logo: {
+    opacity: 1,
+    width: widthPercentageToDP('47.5%'),
+    height: widthPercentageToDP('47.5%'),
+  },
+  loginButton: {
+    width: widthPercentageToDP('89%'),
+    height: heightPercentageToDP('6.5%'),
+    marginTop: heightPercentageToDP('26%'),
+  },
+});
 
 // const firebaseRef = firebase.initializeApp(config);
 
@@ -35,108 +71,70 @@ export default class Signin extends Component {
         style={styles.backgroundImage}
       >
         <LinearGradient
-          colors={['rgba(0, 0, 0, 0.72)', 'rgba(0, 0, 0, 0.11)']}
+          colors={['rgba(0, 0, 0, 0.90)', 'rgba(0, 0, 0, 0.4)']}
           style={styles.linearGradient}
         >
-          <Grid style={styles.container}>
-            <Row
-              size={25}
-              style={{
-                marginTop: 100,
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}
-            >
-              <Image
-                source={require('../assets/img/logo.png')}
-                style={styles.logo}
-                resizeMode="contain"
-              />
-            </Row>
-            <Row size={40} style={{ marginTop: 70 }}>
-              <Text style={styles.welcome}>WhatsMyFood</Text>
-            </Row>
-            <Row size={10}>
-              <LoginButton
-                style={{ width: '88%', height: 45 }}
-                publishPermissions={['publish_actions']}
-                onLoginFinished={(error, result) => {
-                  if (error) {
-                    alert(`Login failed with error: ${result.error}`);
-                  } else if (result.isCancelled) {
-                    alert('Login was cancelled');
-                  } else {
-                    AccessToken.getCurrentAccessToken().then(
-                      accessTokenData => {
-                        const credential = firebase.auth.FacebookAuthProvider.credential(
-                          accessTokenData.accessToken,
+
+          <View style={styles.container}>
+            <Image
+              source={require('../assets/img/logo.png')}
+              style={styles.logo}
+              resizeMode="contain"
+            />
+            <Text style={styles.appTitle}>WHATS MY FOOD</Text>
+
+
+            <LoginButton
+              style={styles.loginButton}
+              publishPermissions={['publish_actions']}
+              onLoginFinished={(error, result) => {
+                if (error) {
+                  alert(`Login failed with error: ${result.error}`);
+                } else if (result.isCancelled) {
+                  alert('Login was cancelled');
+                } else {
+                  AccessToken.getCurrentAccessToken().then(
+                    accessTokenData => {
+                      const credential = firebase.auth.FacebookAuthProvider.credential(
+                        accessTokenData.accessToken,
+                      );
+                      firebase
+                        .auth()
+                        .signInWithCredential(credential)
+                        .then(
+                          () => {
+                            navigation.navigate('Home');
+                          },
+                          // eslint-disable-next-line no-unused-vars
+                          err => {
+                            alert('Error while loggin in');
+                          },
                         );
-                        firebase
-                          .auth()
-                          .signInWithCredential(credential)
-                          .then(
-                            () => {
-                              navigation.navigate('Home');
-                            },
-                            // eslint-disable-next-line no-unused-vars
-                            err => {
-                              alert('Error while loggin in');
-                            },
-                          );
-                      },
-                      err => {
-                        alert(`Some error occured: ${err}`);
-                      },
-                    );
-                  }
-                }}
-                onLogoutFinished={() => {
-                  // delete id from Firebase ?
-                  alert('User logged out');
-                }}
-              />
-            </Row>
-            <Row size={15}>
-              <Text style={{ color: 'white' }}>
+                    },
+                    err => {
+                      alert(`Some error occured: ${err}`);
+                    },
+                  );
+                }
+              }}
+              onLogoutFinished={() => {
+                // delete id from Firebase ?
+                alert('User logged out');
+              }}
+            />
+            <Text style={{ color: 'white' }}>
                 By signing up, I agree with WhatsMyFood’s{'\n'}
-                <Text style={{ color: 'red' }}>
+              <Text style={{ color: 'red' }}>
                   Terms of Service
-                  <Text style={{ color: 'white' }}> and </Text>Privacy policy.
-                </Text>
+                <Text style={{ color: 'white' }}> and </Text>Privacy policy.
               </Text>
-            </Row>
-          </Grid>
+            </Text>
+          </View>
         </LinearGradient>
       </ImageBackground>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  linearGradient: {
-    flex: 1,
-    zIndex: 2,
-  },
-  backgroundImage: {
-    flex: 1,
-    height: null,
-    width: null,
-    zIndex: 1,
-  },
-  container: {
-    zIndex: 3,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  welcome: {
-    fontSize: 30,
-    color: 'white',
-  },
-  logo: {
-    opacity: 1,
-    height: 180,
-  },
-});
 
 Signin.propTypes = {
   navigation: PropTypes.shape({
