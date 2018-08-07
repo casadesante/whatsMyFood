f53c1b0df07ea72e69da47942059b8361831f151
Suresh Murali <m.suresh.fx@gmail.com>
2018-08-07 23:03:10 +0900

    ✨ Feat: Custom Facebook button with Login Manager code to login fb
    Allows user to login automagically with native fb app

--- reactNative/src/components/FbLoginButton.js
+++ reactNative/src/components/FbLoginButton.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, TouchableOpacity, Text } from 'react-native';
 import { LoginManager, AccessToken } from 'react-native-fbsdk';
-// import Icon from 'react-native-vector-icons/MaterialIcons';
-// import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
+import Config from 'react-native-config';
 import firebase from 'firebase';
 import RF from 'react-native-responsive-fontsize';
+import PropTypes from 'prop-types';
 
 import { heightPercentageToDP, widthPercentageToDP } from '../lib/Responsive';
+
+const config = {
+  apiKey: Config.API_KEY,
+  authDomain: Config.AUTH_DOMAIN,
+  databaseURL: Config.DB_URL,
+  storageBucket: 'gs://whatsmyfood.appspot.com',
+};
 
 const styles = StyleSheet.create({
   loginButton: {
@@ -24,16 +31,81 @@ const styles = StyleSheet.create({
   },
 });
 
-const facebookLogin = () => {
-  console.log('Login Manager code');
-};
 
-const FacebookLoginButton = () => (
-  <TouchableOpacity style={styles.loginButton} onPress={facebookLogin}>
-    <Text
-      style={styles.label}
-    > Sign in with FaceBook
-    </Text>
-  </TouchableOpacity>
-);
+class FacebookLoginButton extends Component {
+  componentDidMount() {
+    if (!firebase.apps.length) {
+      firebase.initializeApp(config);
+    }
+  }
+
+  facebookLogin = (navigation) => {
+    async function BM() {
+      console.log('Login Manager code');
+      let result;
+      try {
+        LoginManager.setLoginBehavior('native');
+        result = await LoginManager.logInWithReadPermissions(['public_profile', 'email']);
+      } catch (nativeError) {
+        try {
+          LoginManager.setLoginBehavior('web');
+          result = await LoginManager.logInWithReadPermissions(['public_profile', 'email']);
+        } catch (webError) {
+          alert(`Some error occured: ${webError}`);
+        // show error message to the user if none of the FB screens
+        // did not open
+        }
+      }
+
+
+      if (result.isCancelled) {
+        alert('Login was cancelled');
+      } else {
+        AccessToken.getCurrentAccessToken().then(
+          accessTokenData => {
+            const credential = firebase.auth.FacebookAuthProvider.credential(
+              accessTokenData.accessToken,
+            );
+            firebase
+              .auth()
+              .signInWithCredential(credential)
+              .then(
+                () => {
+                  navigation.navigate('Home');
+                },
+                // eslint-disable-next-line no-unused-vars
+                err => {
+                  alert('Error while loggin in');
+                },
+              );
+          },
+          err => {
+            alert(`Some error occured: ${err}`);
+          },
+        );
+      }
+    }
+    BM();
+    console.log('asdf');
+  }
+
+  render() {
+    const { navigation } = this.props;
+    return (
+      <TouchableOpacity style={styles.loginButton} onPress={() => this.facebookLogin(navigation)}>
+        <Text
+          style={styles.label}
+        > Sign in with FaceBook
+        </Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
 export default FacebookLoginButton;
+
+FacebookLoginButton.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
+};
