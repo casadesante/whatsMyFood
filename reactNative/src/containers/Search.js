199dca33ae4ccfca613732e7b3f837229f322ac1
Suresh Murali <m.suresh.fx@gmail.com>
2018-08-11 17:48:41 +0900

    🎨 Style: Search tab layout

--- reactNative/src/containers/Search.js
+++ reactNative/src/containers/Search.js
@@ -5,6 +5,7 @@ import { StyleSheet,
   ScrollView,
   StatusBar,
   TouchableOpacity } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import PropTypes from 'prop-types';
 import colors from '../lib/Colors';
 import helper from '../lib/Helper'; // to generate sample data. Remove once API is implemented
@@ -20,6 +21,7 @@ const styles = StyleSheet.create({
   restaurantContainer: {
     padding: widthPercentageToDP('4%'),
     paddingBottom: widthPercentageToDP('0%'),
+    paddingTop: widthPercentageToDP('2%'),
   },
   addText: {
     color: 'white',
@@ -32,6 +34,22 @@ const styles = StyleSheet.create({
   },
   headerBackground: {
     backgroundColor: colors.coral,
+  },
+  searchTabContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingTop: heightPercentageToDP('2%'),
+    paddingBottom: heightPercentageToDP('2%'),
+  },
+  tabButton: {
+    width: widthPercentageToDP('50%'),
+  },
+  tabLabel: {
+    fontFamily: 'SFProText-Medium',
+    color: colors.coral,
+    fontSize: RF(3),
+    textAlign: 'center',
   },
 });
 
@@ -53,21 +71,24 @@ export default class Search extends Component {
   };
 
   render() {
-    const { navigation } = this.props;
-
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
         <View style={styles.headerBackground}>
+          <Ionicons name="ios-search" size={35} color={tintColor} />
           <Text style={styles.addText}>Search</Text>
         </View>
-        <View>
-          <TouchableOpacity>
-            <Text>Restaurant</Text>
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <Text>Food</Text>
-          </TouchableOpacity>
+        <View style={styles.searchTabContainer}>
+          <View style={styles.tabButton}>
+            <TouchableOpacity>
+              <Text style={styles.tabLabel}>RESTAURANT</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.tabButton}>
+            <TouchableOpacity>
+              <Text style={styles.tabLabel}>FOOD</Text>
+            </TouchableOpacity>
+          </View>
         </View>
         <ScrollView showsVerticalScrollIndicator={false}>
           <View style={styles.restaurantContainer}>
