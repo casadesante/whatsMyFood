0c3076fa3f1c6512459aae9de04914141ef81ee4
Suresh Murali <m.suresh.fx@gmail.com>
2018-08-12 16:39:23 +0900

    ✨ Feat: Food card, search tab animation  and serach input completed

---
+++ reactNative/src/components/FoodCard.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import { Text,
+  View,
+  ImageBackground,
+  StyleSheet,
+  TouchableOpacity } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import PropTypes from 'prop-types';
+import RF from '../../node_modules/react-native-responsive-fontsize';
+import { heightPercentageToDP, widthPercentageToDP } from '../lib/Responsive';
+
+const styles = StyleSheet.create({
+  linearGradient: {
+    flex: 1,
+    borderRadius: 10,
+  },
+  backgroundImage: {
+    marginTop: 20,
+    height: widthPercentageToDP('92%') * 0.5625,
+  },
+  details: {
+    display: 'flex',
+    height: '100%',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    padding: widthPercentageToDP('4%'),
+  },
+  foodName: {
+    color: 'white',
+    fontFamily: 'SFProText-Bold',
+    fontSize: RF(2.8),
+    textAlign: 'center',
+  },
+  foodRating: {
+    color: 'white',
+    marginTop: heightPercentageToDP('1%'),
+    fontFamily: 'SFProText-Medium',
+    fontSize: RF(5.5),
+    textAlign: 'center',
+  },
+});
+
+const FoodCard = props => {
+  const { food, goToRestaurant } = props;
+  const redGradient = ['rgb(255, 152, 99)', 'rgb(253, 89, 89)'];
+  const blackOverlay = ['rgba(0, 0, 0, 0.50)', 'rgba(0, 0, 0, 0.55)'];
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        goToRestaurant(food.id, food.name);
+      }}
+    >
+      <ImageBackground
+        style={styles.backgroundImage}
+        imageStyle={{ borderRadius: 10 }}
+        resizeMode="contain"
+        source={require('../assets/img/foodImg_16x9.png')
+        // source={{
+          //   uri: food.img,
+          // }}
+        }
+
+      >
+        <LinearGradient
+          colors={food.img ? blackOverlay : redGradient}
+          style={styles.linearGradient}
+        >
+          <View style={styles.details}>
+            <Text style={styles.foodRating}>
+              👌
+            </Text>
+            <Text style={styles.foodName} numberOfLines={2}>
+              Brownie obsession
+            </Text>
+            <Text style={styles.foodName} numberOfLines={2}>
+              in T.G.I.Fridays
+            </Text>
+          </View>
+        </LinearGradient>
+      </ImageBackground>
+    </TouchableOpacity>
+  );
+};
+
+export default FoodCard;
+
+FoodCard.propTypes = {
+  food: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    distance: PropTypes.string,
+    img: PropTypes.string,
+  }),
+  goToRestaurant: PropTypes.func.isRequired,
+};
+
+FoodCard.defaultProps = {
+  food: {
+    distance: '',
+    img: null,
+  },
+};
--- reactNative/src/containers/Search.js
+++ reactNative/src/containers/Search.js
@@ -4,12 +4,16 @@ import { StyleSheet,
   View,
   ScrollView,
   StatusBar,
-  TouchableOpacity } from 'react-native';
+  TouchableOpacity,
+  TextInput } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feather from 'react-native-vector-icons/Feather';
 import PropTypes from 'prop-types';
+import * as Animatable from 'react-native-animatable';
 import colors from '../lib/Colors';
 import helper from '../lib/Helper'; // to generate sample data. Remove once API is implemented
 import RestaurantCard from '../components/RestaurantCard';
+import FoodCard from '../components/FoodCard';
 import { heightPercentageToDP, widthPercentageToDP } from '../lib/Responsive';
 import RF from '../../node_modules/react-native-responsive-fontsize';
 
@@ -23,12 +27,13 @@ const styles = StyleSheet.create({
     paddingBottom: widthPercentageToDP('0%'),
     paddingTop: widthPercentageToDP('2%'),
   },
-  searchPlaceholder: {
+  searchInput: {
     color: 'white',
     fontFamily: 'SFProDisplay-Medium',
     fontSize: RF(3.5),
     letterSpacing: 0.41,
     marginLeft: widthPercentageToDP('4%'),
+    width: widthPercentageToDP('72%'),
   },
   headerBackground: {
     backgroundColor: colors.coral,
@@ -44,12 +49,6 @@ const styles = StyleSheet.create({
   tabButton: {
     width: widthPercentageToDP('50%'),
   },
-  tabLabel: {
-    fontFamily: 'SFProText-Medium',
-    color: colors.coral,
-    fontSize: RF(3),
-    textAlign: 'center',
-  },
   searchBar: {
     display: 'flex',
     flexDirection: 'row',
@@ -59,12 +58,9 @@ const styles = StyleSheet.create({
     borderBottomWidth: 2,
     borderColor: 'white',
   },
-  tabSelector: {
-    height: heightPercentageToDP('0.4%'),
-    backgroundColor: colors.coral,
-    width: widthPercentageToDP('42%'),
-    borderRadius: 100,
-    marginLeft: widthPercentageToDP('8%'),
+  searchIcon: {
+    marginLeft: widthPercentageToDP('1%'),
+    marginTop: heightPercentageToDP('0.2%'),
   },
 });
 
@@ -76,8 +72,9 @@ export default class Search extends Component {
     },
   };
 
-  componentDidMount() {
-    console.log(helper.generateRestaurants());
+  constructor() {
+    super();
+    this.state = { tabState: 'Restaurant', searchKeyword: '' };
   }
 
   goToRestaurant = (id, name) => {
@@ -85,39 +82,106 @@ export default class Search extends Component {
     navigation.navigate('Restaurant', { id, name });
   };
 
+  toggleSearchTab = (selectedTab) => {
+    this.setState({ tabState: selectedTab });
+  }
+
+  tabLabelStyle = (tabName) => {
+    const { tabState } = this.state;
+    const styleObj = {
+      fontFamily: 'SFProText-Medium',
+      color: colors.coral,
+      fontSize: RF(3),
+      textAlign: 'center',
+      opacity: 0.6,
+    };
+    if (tabState === tabName) {
+      styleObj.opacity = 1.0;
+    }
+    return styleObj;
+  }
+
+  tabSelectorStyle = () => {
+    const { tabState } = this.state;
+    const moveLeft = (tabState === 'Restaurant') ? widthPercentageToDP('8%') : widthPercentageToDP('65%');
+    const underlineWidth = (tabState === 'Restaurant') ? widthPercentageToDP('42%') : widthPercentageToDP('20%');
+    return ({
+      height: heightPercentageToDP('0.4%'),
+      backgroundColor: colors.coral,
+      width: underlineWidth,
+      borderRadius: 100,
+      marginLeft: moveLeft,
+    });
+  }
+
+  searchClearStyle = () => {
+    const { searchKeyword } = this.state;
+    const crossOpacity = (searchKeyword === '') ? 0 : 1;
+    return (`rgba(255,255,255,${crossOpacity})`);
+  }
+
   render() {
+    const { tabState, searchKeyword } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
         <View style={styles.headerBackground}>
           <View style={styles.searchBar}>
             <Ionicons name="ios-search" size={RF(4)} color="white" style={styles.searchIcon} />
-            <Text style={styles.searchPlaceholder}>Search</Text>
+            <TextInput
+              style={styles.searchInput}
+              placeholder="Search"
+              placeholderTextColor="rgba(255, 255, 255, 0.7)"
+              onChangeText={(KeyWord) => {
+                this.setState({ searchKeyword: KeyWord });
+              }}
+              text={searchKeyword}
+              selectionColor="white"
+            />
+            <Feather name="x" size={RF(4.4)} color={this.searchClearStyle()} />
           </View>
         </View>
         <ScrollView showsVerticalScrollIndicator={false}>
           <View style={styles.searchTab}>
             <View style={styles.tabButton}>
-              <TouchableOpacity>
-                <Text style={[styles.tabLabel, { marginLeft: widthPercentageToDP('8%') }]}>RESTAURANT</Text>
+              <TouchableOpacity onPress={() => this.toggleSearchTab('Restaurant')}>
+                <Text style={[this.tabLabelStyle('Restaurant'), { marginLeft: widthPercentageToDP('8%') }]}>RESTAURANT</Text>
               </TouchableOpacity>
             </View>
             <View style={styles.tabButton}>
-              <TouchableOpacity>
-                <Text style={styles.tabLabel}>FOOD</Text>
+              <TouchableOpacity onPress={() => this.toggleSearchTab('Food')}>
+                <Text style={this.tabLabelStyle('Food')}>FOOD</Text>
               </TouchableOpacity>
             </View>
           </View>
-          <View style={styles.tabSelector} />
-          <View style={styles.restaurantContainer}>
-            {helper.generateRestaurants().map(restaurantInfo => (
-              <RestaurantCard
-                goToRestaurant={this.goToRestaurant}
-                restaurant={restaurantInfo}
-                key={restaurantInfo.id}
-              />
-            ))}
-          </View>
+          <Animatable.View
+            easing="ease-in-cubic"
+            duration={450}
+            transition={['marginLeft', 'width']}
+            style={this.tabSelectorStyle()}
+          />
+
+          { tabState === 'Restaurant' ? (
+            <View style={styles.restaurantContainer}>
+              {helper.generateRestaurants().map(restaurantInfo => (
+                <RestaurantCard
+                  goToRestaurant={this.goToRestaurant}
+                  restaurant={restaurantInfo}
+                  key={restaurantInfo.id}
+                />
+              ))}
+            </View>) : (
+              <View style={styles.restaurantContainer}>
+                {helper.generateRestaurants().map(foodInfo => (
+                  <FoodCard
+                    goToRestaurant={this.goToRestaurant}
+                    food={foodInfo}
+                    key={foodInfo.id}
+                  />
+                ))}
+              </View>
+          )
+        }
         </ScrollView>
       </View>
     );
