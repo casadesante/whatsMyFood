a38ff315e5868c9cb6ac58cf761fecc7e7663e5a
lakshmantgld <diwaakartg@gmail.com>
2018-08-16 20:25:36 +0900

    ✨ Feat: API for adding Restaurant and food
    NPM dependencies like async, uuid were added. Food and Restaurant will be added to the DB at one
    shot for the first time, a user creates a restaurant

--- serverless/functions/index.js
+++ serverless/functions/index.js
@@ -3,6 +3,9 @@ const functions = require('firebase-functions');
 
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
+const async = require('async');
+const uuidv4 = require('uuid/v4');
+
 admin.initializeApp();
 
 // Get a database reference to whatsMyFood
@@ -10,7 +13,7 @@ var db = admin.database();
 
 exports.addUser = functions.https.onRequest((req, res) => {
   console.log('====================================');
-  console.log("Received Request");
+  console.log("Received Request for adduser");
   console.log(req.body);
   console.log('====================================');
   
@@ -59,6 +62,123 @@ exports.addUser = functions.https.onRequest((req, res) => {
       console.log(err);
       return res.status(500).send(err);
     });
+});
+
+exports.addRestaurantAndFood = functions.https.onRequest((req, res) => {
+  console.log('====================================');
+  console.log("Received Request for addRestaurant");
+  console.log(req.body);
+  console.log('====================================');
+  let parsedRequest = typeof(req.body) === 'object' ? req.body : JSON.parse(req.body);
+
+  // check, If all required parameters are passed.
+  if (!parsedRequest.hasOwnProperty("firebaseID")) {
+    res.status(500).send("No firebaseID in the request");
+  } else if (!parsedRequest.hasOwnProperty("restaurantName")) {
+    res.status(500).send("No restaurantName in the request");
+  } else if (!parsedRequest.hasOwnProperty("food")) {
+    res.status(500).send("No food in the request");
+  }
+
+  var firebaseID = parsedRequest.firebaseID;
+  var readRestaurantRef = db.ref("/restaurants/" + restaurantID);
+  var readUserRef = db.ref("/users/" + firebaseID);
+  var restaurant = {};
+  var restaurantID = null;
+
+  if (parsedRequest.hasOwnProperty("restaurantID")) {
+    restaurantID = parsedRequest.restaurantID;
+  } else {
+    restaurantID = uuidv4();
+  }
+
+  async.waterfall([
+    (callback) => {
+      // check if restaurant exists
+      readRestaurantRef.once("value", (snapshot, readError) => {
+        if (readError) {
+          return callback(readError);
+        }
+        console.log('====================================');
+        console.log("Restaurant snapshot value");
+        console.log(snapshot.val());      
+        console.log('====================================');
+        return callback(null, snapshot.val() !== null);
+      })
+    },
+    (exists, callback) => {
+      if (!exists) {
+        // create restaurant, if it does not exists
+        restaurant[restaurantID] = {
+          "restaurantName": parsedRequest.restaurantName,
+          "latitude": parsedRequest.latitude || null,
+          "longitude": parsedRequest.longitude || null,
+          "createdAt": admin.database.ServerValue.TIMESTAMP,
+          "restaurantPhotoURL": parsedRequest.restaurantPhotoURL || null
+        };
+        console.log('====================================');
+        console.log("printing Restaurant");
+        console.log(restaurant);
+        console.log('====================================');
+        let restaurantsRef = db.ref("/restaurants");
+        restaurantsRef.update(restaurant, (restaurantUpdateError) => {
+          if (restaurantUpdateError) {
+            return callback(restaurantUpdateError);
+          } else {
+            return callback(null, restaurantID);
+          }
+        });
+      } else {
+        return callback(null, restaurantID);
+      }
+    },
+    (restaurantID, callback) => {
+      readUserRef.once("value", (snapshot, readUserError) => {
+        let user = snapshot.val();
+        let refactoredUser = {};
+        if(user.hasOwnProperty('restaurants')) {
+          user.restaurants.push(restaurantID);
+          refactoredUser[firebaseID] = user;
+          return callback(null, refactoredUser);
+        } else {
+          user.restaurants = [restaurantID];
+          refactoredUser[firebaseID] = user;
+          return callback(null, refactoredUser);
+        }
+      })
+    },
+    (refactoredUser, callback) => {
+      let usersRef = db.ref("/users");
+      usersRef.update(refactoredUser, (userUpdateError) => {
+        if (userUpdateError) {
+          return callback(userUpdateError);
+        } else {
+          return callback(null, "Restaurant Successfully added");
+        }
+      })
+    },
+    (emptyMessage, callback) => {
+      let foodsRef = db.ref("/foods");
+      let uniqueFoodKey = foodsRef.push({
+        "foodName": parsedRequest.food.foodName,
+        "foodPhotoURL": parsedRequest.food.foodPhotoURL,
+        "rating": parsedRequest.food.rating,
+        "firebaseID": parsedRequest.firebaseID,
+        "restaurantID": restaurantID
+      });
+      console.log('====================================');
+      console.log(`Food Key: ${uniqueFoodKey}`);
+      console.log('====================================');
+      callback(null, "Added Restaurant & Food");
+    }
+  ], (err, result) => {
+    if (err) {
+      console.log(`error: ${err}`);
+      res.status(500).send(err);
+    }
+    console.log(`result: ${result}`);
+    res.status(200).send(result);
+  });
 });
 
 // Listens for new messages added to /messages/:pushId/original and creates an
