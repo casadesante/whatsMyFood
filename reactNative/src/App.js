fe2b46ab1564c435ab4832fbff2a40f87f0b5127
Suresh Murali <m.suresh.fx@gmail.com>
2018-11-08 13:26:39 -0800

    App.js will call API before loading home page and store restaurants data in AsyncStorage

--- reactNative/src/App.js
+++ reactNative/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, ActivityIndicator } from 'react-native';
+import { StyleSheet, View, ActivityIndicator, AsyncStorage } from 'react-native';
 
 import SplashScreen from 'react-native-splash-screen';
 import { createRootNavigator } from './Routes';
-import { getFromAsyncStorage } from './lib/Auth';
+import { getFromAsyncStorage, getProfileInfo } from './lib/Auth';
 import { heightPercentageToDP } from './lib/Responsive';
 
 const styles = StyleSheet.create({
@@ -25,14 +25,39 @@ export default class App extends Component {
   }
 
   async componentWillMount() {
-    getFromAsyncStorage().then(res => {
-      this.setState({ signedIn: res, promiseResolve: true });
-    });
+    getProfileInfo()
+      .then(user => user.uid)
+      .then(firebaseID => fetch(
+        'https://us-central1-whatsmyfood.cloudfunctions.net/fetchRestaurantsAndFoods',
+        {
+          method: 'POST',
+          body: JSON.stringify({ firebaseID }),
+        },
+      ))
+      .then(restaurants => restaurants.json())
+      .then(parsedRestaurants => this.saveRestaurantsInAsyncStorage(parsedRestaurants))
+      .catch(err => alert(err));
   }
 
   componentDidMount() {
     SplashScreen.hide();
   }
+
+  saveRestaurantsInAsyncStorage = async (restaurants) => {
+    try {
+      await AsyncStorage.setItem(
+        'restaurants',
+        JSON.stringify(restaurants),
+      );
+      getFromAsyncStorage().then(res => {
+        this.setState({ signedIn: res, promiseResolve: true });
+      });
+      return true;
+    } catch (error) {
+      alert('Async store error');
+      return false;
+    }
+  };
 
   render() {
     const { signedIn, promiseResolve } = this.state;
