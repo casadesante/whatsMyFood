fa5cf9347e2aa87161befa9473a5a124fc27eee5
lakshmantgld <diwaakartg@gmail.com>
2019-03-04 10:25:04 +0900

    🔨 Refactor: deleteFood responds with the whole restaurant object

--- serverless/functions/index.js
+++ serverless/functions/index.js
@@ -636,10 +636,15 @@ exports.deleteFood = functions.https.onRequest((req, res) => {
   // check, If all required parameters are passed.
   if (!parsedRequest.hasOwnProperty("foodID")) {
     return res.status(500).send("No foodID in the request");
+  } else if (!parsedRequest.hasOwnProperty("firebaseID")) {
+    return res.status(500).send("No firebaseID in the request");
+  } else if (!parsedRequest.hasOwnProperty("restaurantID")) {
+    return res.status(500).send("No restaurantID in the request");
   }
 
   var deleteFoodRef = db.ref('/foods/' + parsedRequest.foodID);
   var deleteFoodsRef = db.ref('/deleteFoods');
+  var readFoodsRef = db.ref("/foods");
   var retrievedFood = {};
 
   async.waterfall([
@@ -673,7 +678,49 @@ exports.deleteFood = functions.https.onRequest((req, res) => {
       console.log('====================================');
       console.log(`Deleted FoodID: ${deleteFoodUniqueKey}`);
       console.log('====================================');
-      callback(null, parsedRequest.foodID);
+
+      // fetching restaurant based on reastaurantID
+      db.ref("/restaurants/" + parsedRequest.restaurantID).once("value", (snapshot, readRestaurantError) => {
+        if (readRestaurantError) {
+          return callback(readRestaurantError);
+        }
+        let snapshotRestaurant = snapshot.val();
+        // console.log('====================================');
+        // console.log(`typeof: ${typeof(snapshotRestaurant)}`);      
+        // console.log(`Specific Restaurant: ${JSON.stringify(snapshotRestaurant)}`);
+        // console.log('====================================');
+        snapshotRestaurant["restaurantID"] = parsedRequest.restaurantID;
+        console.log('====================================');
+        console.log(JSON.stringify('Printing the restaurant here'));
+        console.log(JSON.stringify(snapshotRestaurant));
+        console.log('====================================');  
+        return callback(null, snapshotRestaurant);
+      }); 
+    },
+    (userRestaurant, callback) => {
+      // fetching foods based on restaurantID & firebaseID
+      readFoodsRef.orderByChild("firebaseID").equalTo(parsedRequest.firebaseID).once("value", (snapshot, readFoodsError) => {
+        if (readFoodsError) {
+          console.log(`readFoodsError: ${readFoodsError}`);
+          throw readFoodsError;
+        }
+        console.log(`Snapshot of foods: ${snapshot}`);
+        let foods = snapshot.val();
+        console.log(`foods: ${JSON.stringify(foods)}`);
+        let filteredFoods = [];
+        for (var key in foods) {
+          if (foods.hasOwnProperty(key)) {
+            let modifiedFood = foods[key];
+            console.log(`modified food: ${JSON.stringify(modifiedFood)}`);
+            if (modifiedFood["restaurantID"] === parsedRequest.restaurantID) {
+              modifiedFood["foodId"] = key;
+              filteredFoods.push(modifiedFood);
+            }
+          }
+        }
+        userRestaurant["foods"] = filteredFoods;
+        return callback(null, userRestaurant);
+      });
     }
   ], (err, result) => {
     if (err) {
